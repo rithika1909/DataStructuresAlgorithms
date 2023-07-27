2577e2a818c1415a423d67abf6daa5c330414409
Rithika L <rithika.l@tvsnext.com>
2023-07-27 12:31:57 +0530

    [Rithika] Add : Implemented

---
+++ DataStructuresAlgorithm/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAlgorithm
+{
+    public class BinarySearch
+    {
+        public void ReadAllTextFileAndSearch(string filePath)
+        {
+            string readAllText = File.ReadAllText(filePath);
+            string[] words = readAllText.Split(",");
+            string key=Console.ReadLine();
+            bool isFind=false; 
+            foreach(var data in words) 
+            {
+                if(data.Equals(key)) 
+                {
+                    isFind = true;
+                }
+               
+            }
+            if (isFind)
+                Console.WriteLine("Search element {0} found", key);
+            else
+                Console.WriteLine("Search element not found");
+        }
+    }
+}
---
+++ DataStructuresAlgorithm/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace DataStructuresAlgorithm
+{
+    class program
+    {
+        static string FilePath = @"D:\DataStructuresAlgorithm\Files\Search.txt";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Option:\n 1.Binary Search\n 2.Exit");
+            
+            bool flag = true;
+            
+            while(flag) 
+            {
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option) 
+                {
+
+                    case 1:
+                        BinarySearch search = new BinarySearch();
+                        Console.WriteLine("Enter Element to search");
+                        search.ReadAllTextFileAndSearch(FilePath);
+                        break;
+                    case 2:
+                        Anagram anagram = new Anagram();
+                        anagram.CheckAnagram();
+                        break;
+                    case 3:
+                        ReplaceString replaceString = new ReplaceString();
+                        replaceString.StringReplace();
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
+                }
+            }
+
+           
+            
+        }
+    }
+}
